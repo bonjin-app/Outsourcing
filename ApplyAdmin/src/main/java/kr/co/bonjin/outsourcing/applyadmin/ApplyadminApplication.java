5435d22c7bbe196d0d4ac55252191528d3e2f4d6
bonjin-all <bonjin.app@gmail.com>
2021-06-18 10:16:08 +0900

    Init Project

---
+++ ApplyAdmin/src/main/java/kr/co/bonjin/outsourcing/applyadmin/ApplyadminApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.outsourcing.applyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyadminApplication.class, args);
+    }
+
+}
---
+++ ApplyAdmin/src/test/java/kr/co/bonjin/outsourcing/applyadmin/ApplyadminApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.outsourcing.applyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
