9253e9cec641764a98c86a76977ad2bad460d360
bonjin-all <bonjin.app@gmail.com>
2021-06-18 15:47:47 +0900

    Add Document: html, nav 연결 수정.

---
+++ ApplyAdmin/src/main/java/kr/co/bonjin/outsourcing/applyadmin/controller/DocumentController.java
@@ -0,0 +1,17 @@
+package kr.co.bonjin.outsourcing.applyadmin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/document")
+@RequiredArgsConstructor
+public class DocumentController {
+
+    @GetMapping
+    public String listPage() {
+        return "document/document-list";
+    }
+}
