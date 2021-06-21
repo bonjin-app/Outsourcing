f7c7a2e62e94ee1dcb537a3f095a2252243f3087
bonjin-all <bonjin.app@gmail.com>
2021-06-21 09:49:02 +0900

    Update SMS: Excel Download 기능 추가.

--- ApplyAdmin/src/main/java/kr/co/bonjin/outsourcing/applyadmin/controller/HistoryController.java
+++ ApplyAdmin/src/main/java/kr/co/bonjin/outsourcing/applyadmin/controller/HistoryController.java
@@ -1,15 +1,25 @@
 package kr.co.bonjin.outsourcing.applyadmin.controller;
 
+import kr.co.bonjin.outsourcing.applyadmin.controller.dto.DocumentResponseDto;
 import kr.co.bonjin.outsourcing.applyadmin.controller.dto.HistoryResponseDto;
 import kr.co.bonjin.outsourcing.applyadmin.controller.dto.PageRequestDto;
 import kr.co.bonjin.outsourcing.applyadmin.controller.dto.PageResultDto;
 import kr.co.bonjin.outsourcing.applyadmin.entity.SmsHistory;
 import kr.co.bonjin.outsourcing.applyadmin.service.HistoryService;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/history")
@@ -29,5 +39,71 @@ public class HistoryController {
         PageResultDto<HistoryResponseDto, SmsHistory> list = historyService.findAllPageable(pageRequestDto);
         model.addAttribute("list", list);
         return "history/sms-list";
+    }
+
+    /**
+     * Excel Download
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping(value = "/excel/download")
+    public void excelDownload(HttpServletResponse response) throws IOException {
+        List<HistoryResponseDto> smsHistory = historyService.findAll();
+
+//        Workbook wb = new HSSFWorkbook();
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("History SMS Sheet");
+        Row row = null;
+        Cell cell = null;
+        int rowNum = 0;
+
+        // Header
+        row = sheet.createRow(rowNum++);
+        cell = row.createCell(0);
+        cell.setCellValue("Number");
+        cell = row.createCell(1);
+        cell.setCellValue("ResultCode");
+        cell = row.createCell(2);
+        cell.setCellValue("Message");
+        cell = row.createCell(3);
+        cell.setCellValue("Receiver");
+        cell = row.createCell(4);
+        cell.setCellValue("MessageID");
+        cell = row.createCell(5);
+        cell.setCellValue("AuthCode");
+        cell = row.createCell(6);
+        cell.setCellValue("IsAush");
+        cell = row.createCell(7);
+        cell.setCellValue("CreateDate");
+
+        // Body
+        for (int i=0; i<smsHistory.size(); i++) {
+            row = sheet.createRow(rowNum++);
+            cell = row.createCell(0);
+            cell.setCellValue(smsHistory.get(i).getId());
+            cell = row.createCell(1);
+            cell.setCellValue(smsHistory.get(i).getResultCode());
+            cell = row.createCell(2);
+            cell.setCellValue(smsHistory.get(i).getMessage());
+            cell = row.createCell(3);
+            cell.setCellValue(smsHistory.get(i).getReceiver());
+            cell = row.createCell(4);
+            cell.setCellValue(smsHistory.get(i).getMessageId());
+            cell = row.createCell(5);
+            cell.setCellValue(smsHistory.get(i).getAuthCode());
+            cell = row.createCell(6);
+            cell.setCellValue(smsHistory.get(i).isAuth());
+            cell = row.createCell(7);
+            cell.setCellValue(smsHistory.get(i).getCreatedDate());
+        }
+
+        // 컨텐츠 타입과 파일명 지정
+        response.setContentType("ms-vnd/excel");
+//        response.setHeader("Content-Disposition", "attachment;filename=example.xls");
+        response.setHeader("Content-Disposition", "attachment;filename=history.xlsx");
+
+        // Excel File Output
+        wb.write(response.getOutputStream());
+        wb.close();
     }
 }
